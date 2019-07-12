2194224cfa1a15951d54291ea8101fb603614f9b
Artyom <artem@anmedio.ru>
2019-07-12 15:38:15 +0400

    React part

---
+++ src/react/components/skillPopup/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const SkillPopup = (d) => {
+	const { data } = d;
+	console.log(data);
+	let links = null;
+	if (data.links) {
+		links = data.links.map(l => {
+			return (
+				<li key={`${data.tag}_${l.name}`}>
+					<a href={l.url} target="_blank">{l.name}</a>
+				</li>
+			)
+		});
+		links = (
+			<React.Fragment>
+				<h4>Ссылки на материалы</h4>
+	  		<ul>
+	  			{links}
+	  		</ul>
+			</React.Fragment>
+		)
+	}
+  return (
+  	<React.Fragment>
+  		<h2>{data.name ? data.name : data.tag}</h2>
+  		<p>{data.text ? data.text : null}</p>
+  		{links}
+  	</React.Fragment>
+  );
+};
+
+export default SkillPopup;
