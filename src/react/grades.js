34f38fefa4975544b898f6e06026a89017231a35
krivtsov <krivtsov.su@gmail.com>
2019-12-08 23:25:18 +0400

    add grade Makefile

--- src/react/grades.js
+++ src/react/grades.js
@@ -300,6 +300,26 @@ const grades = [
             ],
           },
           {
+            tag: 'Makefile',
+            name: 'Makefile',
+            text:
+              'Makefile - файл, содержащий набор инструкций для программы make. Программа make с помощью этого файла позволяет автоматизировать процесс компиляции программы и выполнять при этом различные действия, но на самом деле все проще и полезнее.',
+            links: [
+              {
+                url: 'https://ru.makefile.site/',
+                name: 'Руководство по современному Make',
+              },
+              {
+                url: 'https://github.com/inem/make-handbook/blob/master/modern-make-handbook-ru.md',
+                name: 'modern-make-handbook-ru.md',
+              },
+              {
+                url: 'https://www.youtube.com/watch?v=pK9mF5aK05Q',
+                name: 'Утилита make: полезный универсальный инструмент программиста',
+              },
+            ],
+          },
+          {
             tag: 'ES6+',
             name: 'ES6+',
             text:
@@ -2011,6 +2031,26 @@ const grades = [
               {
                 url: 'https://learn.javascript.ru/screencast/gulp',
                 name: 'Скринкаст Ильи Кантора',
+              },
+            ],
+          },
+          {
+            tag: 'Makefile',
+            name: 'Makefile',
+            text:
+              'Makefile - файл, содержащий набор инструкций для программы make. Программа make с помощью этого файла позволяет автоматизировать процесс компиляции программы и выполнять при этом различные действия, но на самом деле все проще и полезнее.',
+            links: [
+              {
+                url: 'https://ru.makefile.site/',
+                name: 'Руководство по современному Make',
+              },
+              {
+                url: 'https://github.com/inem/make-handbook/blob/master/modern-make-handbook-ru.md',
+                name: 'modern-make-handbook-ru.md',
+              },
+              {
+                url: 'https://www.youtube.com/watch?v=pK9mF5aK05Q',
+                name: 'Утилита make: полезный универсальный инструмент программиста',
               },
             ],
           },
