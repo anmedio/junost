a1380c12a04e4d47e9237a91d44d4f83004630bb
dokugo <dokugo@users.noreply.github.com>
2019-12-12 03:19:43 +0300

    Add CSS-in-JS to Frontend Junior grade

--- src/react/grades.js
+++ src/react/grades.js
@@ -452,6 +452,41 @@ const grades = [
             ],
           },
           {
+            tag: 'CSS-in-JS',
+            name: 'CSS-in-JS',
+            text:
+              'CSS-in-JS — техника стилизации компонентов с помощью JavaScript. Прежде всего позволяет облегчить динамическую стилизацию, сократить количество кода, обеспечить «скоупинг» стилей в компонентах. Наиболее развитые и популярные реализации (Styled-components, Emotion) представлены для библиотеки React.',
+            links: [
+              {
+                url: 'https://jxnblk.com/blog/why-you-should-learn-css-in-js/',
+                name: 'Why you should definitely learn how to use CSS-in-JS',
+              },
+              {
+                url:
+                  'https://dev.to/phizzard/let-s-take-a-look-at-css-in-js-with-react-in-2019-styled-components-1olc',
+                name: 'CSS-in-JS with React in 2019 — Styled Components',
+              },
+              {
+                url: 'https://alert-refactor.netlify.com/',
+                name: 'CSS vs CSS-in-JS (Emotion) example',
+              },
+              {
+                url: 'https://www.gatsbyjs.org/docs/css-in-js/',
+                name: 'Guide on CSS-in-JS usage',
+              },
+              {
+                url:
+                  'https://www.styled-components.com/docs',
+                name: 'Официальная документация Styled-components',
+              },
+              {
+                url:
+                  'https://emotion.sh/docs/introduction',
+                name: 'Официальная документация Emotion',
+              },
+            ],
+          },
+          {
             tag: 'React',
             name: 'React',
             text:
