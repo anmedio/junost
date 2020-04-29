37091e2fe7f0592e8d5da9296b1438b9d0a21549
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:18:53 +0400

    Create J literal tag files

---
+++ src/react/grades/tags/json.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+
+const json = {
+  tag: 'JSON',
+  name: 'JSON',
+  text:
+    'JSON (JavaScript Object Notation) - простой формат обмена данными, удобный для чтения и написания как человеком, так и компьютером. Он основан на подмножестве языка программирования JavaScript, определенного в стандарте ECMA-262 3rd Edition - December 1999. JSON - текстовый формат, полностью независимый от языка реализации, но он использует соглашения, знакомые программистам C-подобных языков, таких как C, C++, C#, Java, JavaScript, Perl, Python и многих других. Эти свойства делают JSON идеальным языком обмена данными.',
+  links: [
+    {
+      url: 'https://www.json.org/json-ru.html',
+      name: 'Официальная документация',
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=bw10M9RuR30',
+      name: 'Sorax - "JSON"',
+    },
+  ],
+};
+
+export default json;
---
+++ src/react/grades/tags/jwt.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+
+const jwt = {
+  tag: 'JWT',
+  name: 'JWT',
+  text:
+    'JWT (JSON Web Token) — это открытый стандарт (RFC 7519) для создания токенов доступа, основанный на формате JSON. Как правило, используется для передачи данных для аутентификации в клиент-серверных приложениях. Токены создаются сервером, подписываются секретным ключом и передаются клиенту, который в дальнейшем использует данный токен для подтверждения своей личности.',
+  links: [
+    {
+      url: 'https://jwt.io/introduction/',
+      name: 'Официальная документация',
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=7nafaH9SddU&t=1063s',
+      name: 'Traversy Media - "Node.js API Authentication With JWT"',
+    },
+  ],
+};
+
+export default jwt;
