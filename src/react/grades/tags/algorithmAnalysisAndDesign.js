034c13cac3b0f97ebb11a0cccbd183f44deee976
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:16:03 +0400

    Create A literal tag files

---
+++ src/react/grades/tags/algorithmAnalysisAndDesign.js
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+
+const algorithmAnalysisAndDesign = {
+  // TODO: Добавить материалы
+  tag: 'Algorithms analysis and design',
+  name: 'Algorithms analysis and design',
+  text: 'Анализ и построение алгоритмов',
+  links: [
+    {
+      url: 'https://e-maxx.ru/bookz/files/cormen.pdf',
+      name: 'Книга "Алгоритмы: Анализ и построение"',
+    },
+  ],
+};
+
+export default algorithmAnalysisAndDesign;
