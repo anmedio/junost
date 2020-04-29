1fdf1226f5a3f63082144fdda67489615504452b
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:18:37 +0400

    Create I literal tag files

---
+++ src/react/grades/tags/iotJs.js
@@ -0,0 +1,22 @@
+/* eslint-disable max-len */
+
+const iotJs = {
+  // TODO: Добавить материал
+  tag: 'IoT',
+  name: 'IoT',
+  text:
+    'IoT (Internet of Things) - интернет вещей — это сеть связанных через интернет объектов, способных собирать данные и обмениваться данными, поступающими со встроенных сервисов. Устройства, входящие в интернет вещей, — любые автономные устройства, подключённые к интернету, которые могут отслеживаться и/или управляться удалённо.',
+  links: [
+    {
+      url:
+        'https://www.youtube.com/playlist?list=PLbW_am_GRTo2O5xTDsVsVX2JX7S4Z6Pvt',
+      name: 'IOT Nodejs',
+    },
+    {
+      url: 'https://github.com/tableflip/nodebot-workshop',
+      name: 'Nodebot Workshop',
+    },
+  ],
+};
+
+export default iotJs;
