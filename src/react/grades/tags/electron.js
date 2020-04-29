f599133c493d3471945b628789f5eebbaa77b819
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:17:37 +0400

    Create E literal tag files

---
+++ src/react/grades/tags/electron.js
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+
+const electron = {
+  // TODO: Добавить материалы
+  tag: 'Electron',
+  name: 'Electron',
+  text:
+    'Electron - фреймворк, разработанный GitHub. Позволяет разрабатывать нативные графические приложения для настольных операционных систем с помощью веб-технологий. Фреймворк включает в себя Node.js для работы с back-end и библиотеку рендеринга из Chromium.',
+  links: [
+    {
+      url: 'https://electronjs.org/docs',
+      name: 'Официальная документация',
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=kN1Czs0m1SU',
+      name: 'Traversy Media - "Build an Electron App in Under 60 Minutes"',
+    },
+    {
+      url: 'https://github.com/maxogden/elementary-electron',
+      name: 'Elementary Electron',
+    },
+  ],
+};
+
+export default electron;
---
+++ src/react/grades/tags/express.js
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+
+const express = {
+  // TODO: Добавить материалы
+  tag: 'Express.js',
+  name: 'Express.js',
+  text:
+    'Express.js — Express - это минималистичный и гибкий веб-фреймворк для приложений Node.js, предоставляющий обширный набор функций для мобильных и веб-приложений. Имея в своем распоряжении множество служебных методов HTTP и промежуточных обработчиков, создать надежный API можно быстро и легко. Express предоставляет тонкий слой фундаментальных функций веб-приложений, которые не мешают вам работать с давно знакомыми и любимыми вами функциями Node.js.',
+  links: [
+    {
+      url: 'https://expressjs.com/en/starter/installing.html',
+      name: 'Официальная документация',
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=L72fhGm1tfE',
+      name: 'Traversy Media - "Express JS Crash Course"',
+    },
+    {
+      url: 'https://github.com/azat-co/expressworks',
+      name: 'Expressworks',
+    },
+  ],
+};
+
+export default express;
