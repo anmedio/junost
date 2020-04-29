97dc68eabf47405913fcbf260c1efe82ed2904b7
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:23:39 +0400

    Create P literal tag files

---
+++ src/react/grades/tags/pythonBasics.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+
+const pythonBasics = {
+  tag: 'Python (basics)',
+  name: 'Python (basics)',
+  text:
+    'Python — высокоуровневый язык программирования общего назначения, ориентированный на повышение производительности разработчика и читаемости кода. Синтаксис ядра Python минималистичен. В то же время стандартная библиотека включает большой объём полезных функций.',
+  links: [
+    {
+      url: 'https://stepik.org/course/512/promo',
+      name: 'Курс на Stepik "Python: основы и применение"',
+    },
+    {
+      url: 'https://www.coursera.org/learn/python-osnovy-programmirovaniya',
+      name: 'Курс на Coursera "Основы программирования на Python"',
+    },
+    {
+      url: 'https://www.coursera.org/specializations/programming-in-python',
+      name: 'Курс на Coursera "Программирование на Python"',
+    },
+    {
+      url: 'https://vk.com/wall-51126445_49142',
+      name: 'Марк Лутц, Изучаем Python',
+    },
+    {
+      url: 'https://pep8.ru/doc/pep8/',
+      name: 'PEP8 — стиль кода в языке Python',
+    },
+  ],
+};
+
+export default pythonBasics;
---
+++ src/react/grades/tags/pythonTooling.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+
+const pythonTooling = {
+  tag: 'Python Tooling',
+  name: 'Python Tooling',
+  text:
+    'Существует множество инструментов, призванных упростить жизнь разработчика и сделать его более эффективным. Глупо не пользоваться ими.',
+  links: [
+    {
+      url: 'https://www.youtube.com/watch?v=qPKmXkxmsHI',
+      name: 'Никита Гришко, «Evolution of dependency management»',
+    },
+    {
+      url: 'https://realpython.com/python-code-quality/',
+      name: 'Python Code Quality: Tools & Best Practices',
+    },
+    {
+      url: 'https://flake8.pycqa.org/en/latest/',
+      name: 'flake8',
+    },
+    {
+      url: 'https://github.com/psf/black',
+      name: 'black',
+    },
+    {
+      url: 'https://realpython.com/python-type-checking/',
+      name: 'Python Type Checking (Guide)',
+    },
+    {
+      url: 'https://habr.com/ru/company/lamoda/blog/432656/',
+      name: 'Введение в аннотации типов Python',
+    },
+  ],
+};
+
+export default pythonTooling;
