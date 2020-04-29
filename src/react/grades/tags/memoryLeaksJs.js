0da30d0ed63ab5da0eccb6725ca382e649f4ad37
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:19:39 +0400

    Create M literal tag files

---
+++ src/react/grades/tags/memoryLeaksJs.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+
+const memoryLeaksJs = {
+  tag: 'Memory leaks',
+  name: 'Memory leaks',
+  text:
+    'Утечки памяти - (англ. memory leak) — процесс неконтролируемого уменьшения объёма свободной оперативной или виртуальной памяти компьютера, связанный с ошибками в работающих программах, вовремя не освобождающих ненужные участки памяти, или с ошибками системных служб контроля памяти.',
+  links: [
+    {
+      url: 'https://medium.com/webbdev/js-ae40719fd84e',
+      name:
+        'Статья на Medium - "Управление памятью в JS, четыре вида утечек памяти"',
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=0oZa64SB2wM',
+      name:
+        'Тимур Шемсединов - "Утечки памяти в Node.js и JavaScript, сборка мусора и профилирование"',
+    },
+    {
+      url: 'https://learn.javascript.ru/optimize',
+      name:
+        'Учебник Ильи Кантора "learn.javascript.ru" - Утечки памяти и оптимизации',
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=y_rVMy_5wz0',
+      name:
+        'Vladislav Orlikov - "Автоматизация поиска утечек памяти в клиентском JavaScript"',
+    },
+  ],
+};
+
+export default memoryLeaksJs;
