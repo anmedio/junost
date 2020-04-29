b5e82b1870f6411293aa1ce47a3d4a680b914c17
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:19:55 +0400

    Create N literal tag files

---
+++ src/react/grades/tags/nextjs.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+
+const nextjs = {
+  tag: 'Next.js',
+  name: 'Next.js',
+  text:
+    'Next.js представляет собой облегченный фреймворк для статических и обработанных сервером приложений React. По сути это Server Side Rendering для React с возможностью генерации статиеских сборок.',
+  links: [
+    {
+      url: 'https://nextjs.org/docs',
+      name: 'Официальная документация',
+    },
+    {
+      url:
+        'https://medium.com/@ivaneroshkin/next-js-server-side-rendering-%D0%B4%D0%BB%D1%8F-react-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-f5f61e20dc8c',
+      name: 'Статья на Medium',
+    },
+  ],
+};
+
+export default nextjs;
---
+++ src/react/grades/tags/npm.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+
+const npm = {
+  tag: 'NPM',
+  name: 'NPM',
+  text: 'NPM — менеджер пакетов, входящий в состав Node.js.',
+  links: [
+    {
+      url: 'https://docs.npmjs.com',
+      name: 'Официальная документация',
+    },
+    {
+      url: 'https://coursehunters.net/course/znakomstvo-s-npm',
+      name: 'Курс "Знакомство с npm"',
+    },
+  ],
+};
+export default npm;
