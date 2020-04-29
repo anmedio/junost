59fc1822572ceacfeaf3a1d26c8b4310b4e19722
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:17:49 +0400

    Create F literal tag files

---
+++ src/react/grades/tags/figma.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+
+const figma = {
+  tag: 'Figma',
+  name: 'Figma',
+  text:
+    'Figma — кросс-платформенный онлайн-сервис для дизайнеров интерфейсов и веб-разработчиков. Figma позволяет разрабатывать интерфейсы в онлайн-приложении. У Figma две ключевые особенности: доступ к макету прямо из окна браузера и возможность совместной работы над документами. Вам, как веб-разработчику, необходимо уметь работать с макетами, которые Вы получаете от дизайнера. Figma прекрасно подходит для этой работы.',
+  links: [
+    {
+      url: 'https://www.figma.com/',
+      name: 'Официальный сайт Figma',
+    },
+    {
+      url: 'https://htmlacademy.ru/blog/useful/figma',
+      name: 'HTMLacademy - "Старт в Figma для верстальщика"',
+    },
+    {
+      url: 'https://coursehunters.net/course/izuchaem-figma',
+      name: 'Курс "Mastering Figma"',
+    },
+  ],
+};
+
+export default figma;
