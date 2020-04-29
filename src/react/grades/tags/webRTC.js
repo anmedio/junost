302e6572bc591fefdcae30c165c8b3692e5b5055
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:24:53 +0400

    Create W literal tag files

---
+++ src/react/grades/tags/webRTC.js
@@ -0,0 +1,22 @@
+/* eslint-disable max-len */
+
+const webRTC = {
+  // TODO: Добавить материал
+  tag: 'WebRTC',
+  name: 'WebRTC',
+  text:
+    'WebRTC (Web Real Time Communications) — это стандарт, который описывает передачу потоковых аудиоданных, видеоданных и контента от браузера и к браузеру в режиме реального времени без установки плагинов или иных расширений. Стандарт позволяет превратить браузер в оконечный терминал видеоконференцсвязи, достаточно просто открыть веб-страницу, чтобы начать общение.',
+  links: [
+    {
+      url:
+        'https://www.youtube.com/playlist?list=PL1QRvYV-LXn7Mdt7pKOdp7gircj8CwMgk',
+      name: "Let's Write Code - P2P / WebRTC",
+    },
+    {
+      url: 'https://trueconf.ru/webrtc.html',
+      name: 'Trueconf - WebRTC',
+    },
+  ],
+};
+
+export default webRTC;
