6309285b4c97670c463ac8b074dd630581e92bb0
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:19:19 +0400

    Create L literal tag files

---
+++ src/react/grades/tags/linuxBasics.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+
+const linuxBasics = {
+  tag: 'Linux (basics)',
+  name: 'Linux (basics)',
+  text:
+    'Linux - семейство Unix-подобных операционных систем на базе ядра Linux, включающих тот или иной набор утилит и программ проекта GNU, и, возможно, другие компоненты. Понимание основ Linux и умение пользоваться системой - важный аспект в работе веб-разработчика.',
+  links: [
+    {
+      url: 'https://www.youtube.com/watch?v=tQLpAefAKuA',
+      name:
+        'Хороший программист - "Основы Ubuntu Linux: apt-get, bash, командная строка"',
+    },
+    {
+      url:
+        'https://www.youtube.com/playlist?list=PL874KddjzYd8d_A1mmUwxZ63MtJaSBXE1',
+      name: 'Linux для Начинающих',
+    },
+    {
+      url: 'https://github.com/denysdovhan/learnyoubash',
+      name: 'Learnyoubash',
+    },
+  ],
+};
+
+export default linuxBasics;
