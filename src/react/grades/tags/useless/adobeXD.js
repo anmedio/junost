dc199aecc069cb17a9ef1ea5d4bfc5abd03b0f92
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:13:35 +0400

    Remove useless tags from the specs and move them to the files

---
+++ src/react/grades/tags/useless/adobeXD.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+
+const adobeXD = {
+  tag: 'Adobe XD',
+  name: 'Adobe XD',
+  text:
+    'Adobe XD - программа для разработки интерфейсов от Adobe Systems. Поддерживает векторную графику и веб-верстку и создает небольшие активные прототипы. Как и Figma подходит для работы с макетами, которые получает веб-разработчик от дизайнера.',
+  links: [
+    {
+      url: 'https://www.adobe.com/ru/products/xd.html',
+      name: 'Официальный сайт Adobe XD',
+    },
+    {
+      url: 'https://coursehunters.net/course/adobe-xd-uskorennyy-kurs',
+      name: 'Курс "Adobe XD Crash Course"',
+    },
+  ],
+};
+
+export default adobeXD;
---
+++ src/react/grades/tags/useless/mariaDB.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+
+const mariaDB = {
+  tag: 'MariaDB',
+  name: 'MariaDB',
+  text:
+    'MariaDB — ответвление от системы управления базами данных MySQL, разрабатываемое сообществом под лицензией GNU GPL. Разработку и поддержку MariaDB осуществляет компания MariaDB Corporation Ab и фонд MariaDB Foundation. Толчком к созданию стала необходимость обеспечения свободного статуса СУБД, в противовес политике лицензирования MySQL компанией Oracle. Основателями проекта выступили первоначальные разработчики MySQL. Система лицензирования MariaDB обязывает участников, желающих добавить свой код в основную ветку СУБД, обмениваться своими авторскими правами с MariaDB Foundation для охраны лицензии и возможности создавать критические исправления для MySQL.',
+  links: [
+    {
+      url: 'https://mariadb.com/kb/en/library/documentation/',
+      name: 'Официальная документация',
+    },
+    {
+      url:
+        'https://www.youtube.com/playlist?list=PL9Uyqhstp-wNllCTzTu7gDdLRTAJ4TQ_p',
+      name: 'MariaDB Corporation - "Getting Started with MariaDB "',
+    },
+  ],
+};
+
+export default mariaDB;
