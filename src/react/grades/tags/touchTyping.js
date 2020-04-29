ffbd55afc6d5ef5faae7d5ea487bd9dce9dc6132
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:24:22 +0400

    Create T literal tag files

---
+++ src/react/grades/tags/touchTyping.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+
+const touchTyping = {
+  tag: 'Touch typing',
+  name: 'Touch typing',
+  text:
+    'Слепой метод набора (слепая печать) — методика набора текста «вслепую», то есть не глядя на клавиши печатной машинки или клавиши клавиатуры, используя все (или большинство) пальцы рук.',
+  links: [
+    {
+      url: 'https://klava.org/delta/#rus_speed',
+      name: 'Klava.org',
+    },
+    {
+      url: 'https://klavogonki.ru/',
+      name: 'Клавагонки',
+    },
+  ],
+};
+
+export default touchTyping;
