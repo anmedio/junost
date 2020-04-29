b1668743dde69826867729c3467368802f373fb1
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:15:15 +0400

    Start to create a single files for tags. Create ajax tag file.

---
+++ src/react/grades/tags/ajax.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+
+const ajax = {
+  tag: 'AJAX',
+  name: 'AJAX',
+  text:
+    'AJAX - (ˈeɪdʒæks, от англ. Asynchronous JavaScript and XML — «асинхронный JavaScript и XML») — подход к построению интерактивных пользовательских интерфейсов веб-приложений, заключающийся в «фоновом» обмене данными браузера с веб-сервером. В результате, при обновлении данных, веб-страница не перезагружается полностью, и веб-приложения становятся быстрее и удобнее. По-русски иногда произносится транслитом как «аякс». У аббревиатуры AJAX нет устоявшегося аналога на кириллице.',
+  links: [
+    {
+      url: 'https://developer.mozilla.org/ru/docs/Web/Guide/AJAX',
+      name: 'Документация MDN',
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=lEfp6_ByY-Q',
+      name: 'Loftblog - "AJAX. Настройка и отправка AJAX запросов"',
+    },
+    {
+      url:
+        'https://www.youtube.com/watch?v=wMMki2FEYGY&list=PLHhi8ymDMrQZad6JDh6HRzY1Wz5WB34w0&index=53',
+      name:
+        'Тимур Шемсединов - "HTTP запросы в браузере и Node.js: XMLHttpRequest, fetch"',
+    },
+  ],
+};
+
+export default ajax;
