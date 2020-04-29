707d03511b425add87ff389378b35991c388b6f7
nikolasmelui <slak@samaradom.ru>
2020-04-29 19:18:20 +0400

    Update the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,10 @@ const pug = require('gulp-pug'),
   cleanCSS = require('gulp-clean-css'),
   del = require('del');
 
-const clean = () => {
-  return del('docs/*');
-};
+const clean = () => del('docs/*');
 
-const html = () => {
-  return src('src/pug/*.pug')
+const html = () =>
+  src('src/pug/*.pug')
     .pipe(
       plumber({
         errorHandler: notify.onError('👻 <%= error.message %>'),
@@ -34,10 +32,9 @@ const html = () => {
     .pipe(pug({ pretty: true }))
     .pipe(dest('docs/'))
     .pipe(reload({ stream: true }));
-};
 
-const css = () => {
-  return src('src/scss/main.scss')
+const css = () =>
+  src('src/scss/main.scss')
     .pipe(
       plumber({
         errorHandler: notify.onError('👻 <%= error.message %>'),
@@ -54,10 +51,9 @@ const css = () => {
     .pipe(sourcemaps.write('./'))
     .pipe(dest('docs/static/css/'))
     .pipe(reload({ stream: true }));
-};
 
-const js = () => {
-  return src('src/js/*.js')
+const js = () =>
+  src('src/js/*.js')
     .pipe(
       plumber({
         errorHandler: notify.onError('👻 <%= error.message %>'),
@@ -70,37 +66,31 @@ const js = () => {
     )
     .pipe(dest('docs/static/js'))
     .pipe(reload({ stream: true }));
-};
 
-const fonts = () => {
-  return src('src/fonts/**/*.*').pipe(dest('docs/static/fonts'));
-};
+const fonts = () => src('src/fonts/**/*.*').pipe(dest('docs/static/fonts'));
 
-const images = () => {
-  return src('src/img/**/*.*')
+const images = () =>
+  src('src/img/**/*.*')
     .pipe(
       imgmin([pngquant(), mozjpeg(), svgo({ removeViewBox: false })], {
         verbose: true,
       }),
     )
     .pipe(dest('docs/static/img'));
-};
 
-const assetsCss = () => {
-  return src('src/assets/**/*.css')
+const assetsCss = () =>
+  src('src/assets/**/*.css')
     .pipe(concat('assets.css'))
     .pipe(cleanCSS())
     .pipe(dest('./docs/static/assets'))
     .pipe(reload({ stream: true }));
-};
 
-const assetsJs = () => {
-  return src('src/assets/**/*.js')
+const assetsJs = () =>
+  src('src/assets/**/*.js')
     .pipe(concat('assets.js'))
     .pipe(uglify())
     .pipe(dest('./docs/static/assets'))
     .pipe(reload({ stream: true }));
-};
 
 const watching = () => {
   watch('src/pug/**/*.pug', html);
@@ -120,13 +110,13 @@ const browser = () => {
   });
 };
 
+exports.watch = watching;
+exports.clean = clean;
 exports.build = series(
   clean,
   parallel(html, css, js),
   parallel(fonts, images, parallel(assetsCss, assetsJs)),
 );
-exports.watch = watching;
-exports.clean = clean;
 exports.default = series(
   clean,
   parallel(html, css, js),
