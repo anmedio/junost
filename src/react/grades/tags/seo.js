7e5fa944888826a4352535fe02dd0419b7d2f426
nikolasmelui <slak@samaradom.ru>
2020-04-30 00:24:13 +0400

    Create S literal tag files

---
+++ src/react/grades/tags/seo.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+
+const seo = {
+  // TODO: Добавить материал
+  tag: 'SEO',
+  name: 'SEO',
+  text:
+    'SEO (Search Engine Optimization, поисковая оптимизация) – это всестороннее развитие и продвижение сайта для его выхода на первые позиции в результатах выдачи поисковых систем (SERPs) по выбранным запросам с целью увеличения посещаемости и дальнейшего получения дохода.',
+  links: [
+    {
+      url: 'https://seo.ru/chto-takoe-seo/#one',
+      name: 'Статья на сайте SEO.ru',
+    },
+  ],
+};
+
+export default seo;
