43872db3e196240dbb2c1856a4dc7fa60fda51e6
nikolasmelui <slak@samaradom.ru>
2020-04-30 13:54:31 +0400

    Create webframework java tag

---
+++ src/react/grades/tags/webFrameworkJava.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+
+const webFrameworkJava = {
+  tag: 'Web Framework',
+  name: 'Web Framework',
+  text:
+    'Современный темп разработки и запросы бизнеса требуют комплексных решений, поэтому использование Web фреймворков является неотъемлемой частью процесса. Самые популярные решения среди Java разработчиков это Jakarta EE и Spring, но не стоит забывать об альтернативах. При выборе фреймворка ориентируйтесь на его востребованность, поддержку сообщества, функционал и индивидуальные предпочтения.',
+  links: [
+    {
+      url: 'https://spring.io/guides',
+      name: 'Official Spring Guides',
+    },
+    {
+      url: 'https://jakarta.ee/release/',
+      name: 'Jakarta EE Resources',
+    },
+    {
+      url: 'https://quarkus.io/get-started/',
+      name: 'Quarkus Get Started',
+    },
+    {
+      url: 'https://vertx.io/docs/',
+      name: 'Vert.x Documentation',
+    },
+    {
+      url: 'https://javalin.io/documentation',
+      name: 'Javalin Documentation',
+    },
+  ],
+};
+
+export default webFrameworkJava;
